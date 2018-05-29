f30dd8a1c9d1700c8e240d5833f6131d356ec02d
Joshua Ong <ong.joshuajames@gmail.com>
2018-05-29 23:31:23 +0100

    Riddle 4 change to use scan

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle4.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle4.kt
@@ -4,13 +4,12 @@ import io.reactivex.Observable
 
 object Riddle4 {
 
-  private var count = 0
-
-  /**
-   * Implement a toggle mechanism. Initially we want to return false.
-   * Every time [source] emits, we want to negate the previous value.
-   *
-   * Use case: Some button that can toggle two states. For instance a switch between White & Dark theme.
-   */
-  fun solve(source: Observable<Unit>): Observable<Boolean> = source.map { count % 2 != 0 }
+    /**
+     * Implement a toggle mechanism. Initially we want to return false.
+     * Every time [source] emits, we want to negate the previous value.
+     *
+     * Use case: Some button that can toggle two states. For instance a switch between White & Dark theme.
+     */
+    fun solve(source: Observable<Unit>): Observable<Boolean> = source.scan(false, { acc, _ -> !acc })
+    
 }
