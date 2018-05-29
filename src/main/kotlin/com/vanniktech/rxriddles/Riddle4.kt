ba6084986c563f8d310ac96266653a2561fb4b2a
Joshua Ong <ong.joshuajames@gmail.com>
2018-05-29 23:37:03 +0100

    Riddle 5

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle4.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle4.kt
@@ -11,5 +11,5 @@ object Riddle4 {
      * Use case: Some button that can toggle two states. For instance a switch between White & Dark theme.
      */
     fun solve(source: Observable<Unit>): Observable<Boolean> = source.scan(false, { acc, _ -> !acc })
-    
+
 }
